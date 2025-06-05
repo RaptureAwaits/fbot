e1925aa78ed29945552e6b2dd66101f4cb23cd46
RaptureAwaits <fredericsmythe512@gmail.com>
2025-06-06 00:28:26 +0100

    Improved format of uptime command response

--- src/modules/misc.py
+++ src/modules/misc.py
@@ -2,6 +2,7 @@ from discord import Colour
 from discord.ext.commands import Cog, command, Context
 
 from datetime import datetime, timedelta
+from math import floor
 
 from src.constants import APP_NAME, SOURCE_ICON, SOURCE_URL
 from src.extensions import AlertEmbed
@@ -30,10 +31,30 @@ class MiscCog(Cog, name="Miscellaneous"):
     @command("uptime")
     async def uptime_command(self, context: Context):
         uptime_td: timedelta = datetime.now() - start_time
-        uptime_str = f"Current uptime: {str(uptime_td)}"
+
+        s = uptime_td.total_seconds()
+        years, rem = divmod(s, 60 * 60 * 24 * 365)
+        days, rem = divmod(rem, 60 * 60 * 24)
+        hours, rem = divmod(rem, 60 * 60)
+        mins, secs = divmod(rem, 60)
+        uptime_str = (
+            f"Current uptime:\n" +
+            f"- {floor(years):02} years,\n" +
+            f"- {floor(days):02} days,\n"
+            f"- {floor(hours):02} hours,\n"
+            f"- {floor(mins):02} minutes, and\n"
+            f"- {floor(secs):02} seconds."
+        )
+
         uptime_embed = AlertEmbed(
             title=f"{APP_NAME} Uptime Report",
             description=uptime_str,
             msg=context.message
         )
+
+        th = {1: "st", 2: "nd", 3: "rd"}.get(start_time.day, "th")
+        footer_str = (
+            f"This instance has been live since {start_time.strftime(f'%A %d{th} %B %Y, %H:%M:%S')}."
+        )
+        uptime_embed.set_footer(text=footer_str)
         await uptime_embed.send(context.channel)
